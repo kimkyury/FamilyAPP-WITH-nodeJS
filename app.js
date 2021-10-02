a3f20a0a98e8888737a70d97756bd9b0a0925e90
kimkyury <mandarining0918@gmail.com>
2021-10-02 23:47:23 +0900

    Feat: html 파일 연결

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express"); //서버역할
+const path = require("path"); //html파일 연결을 위한 path모듈
 
 const app = express();
 app.set("port", process.env.PORT || 3000); // 서버가 실행될 포트 설정
 
 app.get("/", (req, res) => {
   // 기본 주소에 GET요청시 동작 설정
-  res.send("FamilyApp-WITH"); // express에선 res.write, res.end대신 res.send를 사용
+  //res.send('WITH-Sever-Ofen') // express에선 res.write, res.end대신 res.send를 사용
+  res.sendFile(path.join(__dirname, "/index.html")); //html파일 연결을 위한 sendFile함수
 });
 
 app.listen(app.get("port"), () => {
