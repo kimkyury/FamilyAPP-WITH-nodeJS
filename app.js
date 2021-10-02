c96539b054e08e5f8256f0bf61ad7089b2a97387
kimkyury <mandarining0918@gmail.com>
2021-10-02 23:42:23 +0900

    Feat: 서버오픈

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require("express"); //서버역할
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000); // 서버가 실행될 포트 설정
 
 app.get("/", (req, res) => {
-  res.send("FamilyApp-WITH");
+  // 기본 주소에 GET요청시 동작 설정
+  res.send("FamilyApp-WITH"); // express에선 res.write, res.end대신 res.send를 사용
 });
 
 app.listen(app.get("port"), () => {
