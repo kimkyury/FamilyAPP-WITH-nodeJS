dc8f7672491e5ceb7b7aa2534ded4721836e6fbb
kimkyury <mandarining0918@gmail.com>
2021-10-11 21:17:48 +0900

    Feat: mongoDB 연결하기

---
+++ learn-sequelize/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+
+/* sequelize와 sync메서드를 통해 서버 실행 시 MySQL와 연동 */
+const { sequelize} = require ('./models'); // require('./models/index.js')와 같음, 폴더 내 index.js파일은 require시 이름 생략 가능
+
+const app = express();
+app.set('port', process.env.PORT || 3001);
+app.set('view engine', 'html');
+
+
+nunjucks.configure('views', {
+    express:app,
+    watch:true,
+});
+sequelize.sync({ force :false}) // true로 설정시, 서버 실행 시마다 테이블을 재생성함. 테이블 잘 못 만들었으면 true로 하기.
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+})
+.catch(err) => {
+    console.error(err);
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(exprses.urlencoded({ extended:false}));
+
+app.use((req, res, next ) => {
+    const error = new Error(`${req.method} ${req.url}라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+app.listen(app.get('port'); () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
+ㅐ
+
