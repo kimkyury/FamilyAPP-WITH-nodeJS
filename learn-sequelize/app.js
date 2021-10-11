06f8c1b76ed48cbd8ba369df741077f9130d9e2c
kimkyury <mandarining0918@gmail.com>
2021-10-11 21:35:07 +0900

    Chore: 오타수정

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -1,41 +1,46 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
-
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
 
 /* sequelize와 sync메서드를 통해 서버 실행 시 MySQL와 연동 */
-const { sequelize} = require ('./models'); // require('./models/index.js')와 같음, 폴더 내 index.js파일은 require시 이름 생략 가능
+const { sequelize } = require("./models"); // require('./models/index.js')와 같음, 폴더 내 index.js파일은 require시 이름 생략 가능
 
 const app = express();
-app.set('port', process.env.PORT || 3001);
-app.set('view engine', 'html');
+app.set("port", 3001); //app.set("port", process.env.PORT || 3001);
+app.set("view engine", "html");
 
-
-nunjucks.configure('views', {
-    express:app,
-    watch:true,
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
 });
-sequelize.sync({ force :false}) // true로 설정시, 서버 실행 시마다 테이블을 재생성함. 테이블 잘 못 만들었으면 true로 하기.
-.then(() => {
-    console.log('데이터베이스 연결 성공');
-})
-.catch(err) => {
+sequelize
+  .sync({ force: false }) // true로 설정시, 서버 실행 시마다 테이블을 재생성함. 테이블 잘 못 만들었으면 true로 하기.
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
     console.error(err);
-});
+  });
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(exprses.urlencoded({ extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next ) => {
-    const error = new Error(`${req.method} ${req.url}라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url}라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
-app.listen(app.get('port'); () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
-});
-ㅐ
 
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
