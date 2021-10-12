f6de31443cc4cc68c694fe924230d01856a3ea47
kimkyury <mandarining0918@gmail.com>
2021-10-12 14:52:47 +0900

    Feat: 시퀄라이즈와 MySQL 모델 연결

--- learn-sequelize/models/index.js
+++ learn-sequelize/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require("sequelize");
+const User = require("./user");
+const Comment = require("./comment");
 
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
@@ -11,5 +13,14 @@ const sequelize = new Sequelize(
   config
 );
 db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize); //init가 실행되어야 테이블이 모델로 연결 됨
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
