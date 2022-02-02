e5c9826cc7a18ea2ddac3d860883e96bb9e1c7c6
kgyury <mandarining0918@gmail.com>
2022-02-02 21:32:28 +0900

    feat: frontend init setting

--- frontend/middleware/authenticated.js
+++
@@ -1,5 +0,0 @@
-export default function ({ store, redirect }) {
-    if (!store.state.users.me) {
-        redirect('/');
-    }
-}
---
+++ frontend/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Conatiner -->
+    <div
+    align="center"
+    justify="center"
+    class="py-3"
+    style="margin: auto; margin-top: 3%"
+  >
+      <input ref="imageInput" type="file" hidden @change="onChangeImage" />
+    <v-btn
+      fab
+      elevation="0"
+      color="#11D600"
+      type="button"
+      @click="onClickImageUpload"
+      ><v-icon x-large color="white">mdi-plus</v-icon></v-btn
+    >
+    <br />
+    </div>
+
+</template>
+
